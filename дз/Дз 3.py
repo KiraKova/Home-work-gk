871e87a1c4e849986e8cbeb4d56cefacfb032f51
Kira <97297341+KiraKova@users.noreply.github.com>
2022-02-17 21:54:18 +0700

    Delete дз directory

--- дз/Дз 3.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Введите любое число: "))
-n = str(n)
-n1 = n
-n2 = n+n
-n3 = n+n+n
-print(int(n1)+int(n2)+int(n3))
--- дз/Дз4.py
+++
@@ -1,8 +0,0 @@
-ch = int(input("Введите число: "))
-x = 0
-while(ch):
-    if ch%10 > x:
-        x = ch%10
-    ch//=10
-
-print(x)
